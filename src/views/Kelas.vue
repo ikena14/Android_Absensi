219d8549c814e1be53bbe0d4c4662b67d3331ee8
Ubuntu <ubuntu@ip-172-31-11-35.ec2.internal>
2021-01-15 05:02:21 +0000

    Membuat tampilan login, kelas dan detail kelas

---
+++ src/views/Kelas.vue
@@ -0,0 +1,80 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar color="dark">
+                <ion-title>Kelas</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <ion-list>
+                <ion-item>
+                    <ion-label>
+                        Kelas 1
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata kuliah</p>
+                    </ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>
+                        Kelas 2
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata kuliah</p>
+                    </ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>
+                        Kelas 3
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata kuliah</p>
+                    </ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>
+                        Kelas 4
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata kuliah</p>
+                    </ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>
+                        Kelas 5
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata kuliah</p>
+                    </ion-label>
+                </ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+  import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonList
+} from '@ionic/vue'
+import{defineComponent}from 'vue'
+export default defineComponent({
+    components:{
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonItem,
+        IonLabel,
+        IonList
+    }
+})
+</script>
+
+<style scoped>
+    .rata-kanan{
+        float:right;
+    }
+</style>
